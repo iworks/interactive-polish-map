0c54f5b0e473c0de03545f3874eeb0a07bf6f068
Marcin Pietrzak <marcin@iworks.pl>
2021-06-26 11:20:28 +0200

    working on svg version

--- includes/iworks/class-interactive-polish-map.php
+++ includes/iworks/class-interactive-polish-map.php
@@ -29,38 +29,114 @@ require_once dirname( dirname( __FILE__ ) ) . '/iworks.php';
 
 class iworks_interactive_polish_map extends iworks {
 
+	private $provinces;
+	private $options;
+	private $blocks;
+	private $capability;
+	private $settings_page;
+
 	public function __construct() {
-		add_action( 'admin_menu', array( $this, 'add_pages' ) );
+		parent::__construct();
+		/**
+		 * Settings
+		 */
+		$this->options    = iworks_interactive_polish_map_get_options_object();
+		$this->base       = dirname( dirname( __FILE__ ) );
+		$this->dir        = basename( dirname( $this->base ) );
+		$this->version    = 'PLUGIN_VERSION';
+		$this->capability = apply_filters( 'interactive_polish_map_capability', 'manage_options' );
+		/**
+		 * WordPress Hooks
+		 */
 		add_action( 'admin_init', array( $this, 'admin_init' ) );
 		add_action( 'init', array( $this, 'init' ) );
+		/**
+		 * WordPress Shortcodes
+		 */
 		add_shortcode( 'mapa-polski', array( $this, 'shortcode' ) );
-	}
-
-	public function admin_init() {
-		global $ipm_data;
-		foreach ( array_keys( $ipm_data['districts'] ) as $key ) {
-			register_setting( 'ipm-options', 'ipm_districts_' . $key, 'wp_filter_nohtml_kses' );
-		}
-		register_setting( 'ipm-options', 'ipm_type', 'absint' );
-		register_setting( 'ipm-options', 'ipm_menu', 'wp_filter_nohtml_kses' );
-	}
-
-	public function add_pages() {
-		add_submenu_page(
-			'options-general.php',
-			__( 'Interactive Polish Map', 'interactive_polish_map' ),
-			__( 'Interactive Polish Map', 'interactive_polish_map' ),
-			'edit_posts',
-			'ipm_settings',
-			'ipm_settings'
-		);
+		add_shortcode( 'interactive-polish-map', array( $this, 'shortcode' ) );
+		/**
+		 * iWorks Rate Class
+		 */
+		add_filter( 'iworks_rate_notice_logo_style', array( $this, 'filter_plugin_logo' ), 10, 2 );
+		add_filter( 'iworks_rate_settings_page_url_' . 'interactive-polish-map', array( $this, 'filter_get_setting_page_url' ) );
 	}
 
 	public function shortcode() {
-		global $ipm_data;
-		$content = sprintf( '<div id="ipm_type_%d"><ul id="w" class="%s">', get_option( 'ipm_type', 500 ), get_option( 'ipm_menu', 'ponizej' ) );
-		$i       = 1;
-		foreach ( $ipm_data['districts'] as $key => $value ) {
+		/**
+		 * get map
+		 */
+		$file = dirname( $this->base ) . '/assets/images/map-of-poland.svg';
+		ob_start();
+		include $file;
+		$map = ob_get_clean();
+		if ( empty( $map ) ) {
+			return;
+		}
+		/**
+		 * border color
+		 */
+		$border_color = $this->options->get_option( 'border' );
+		$map          = preg_replace( '/"#ddd"/', $border_color, $map );
+		/**
+		 * provinces colors
+		 */
+		$style          = $this->options->get_option( 'style' );
+		$color1         = $this->options->get_option( 'color1' );
+		$color1_opacity = intval( $this->options->get_option( 'color1_opacity' ) ) / 100;
+
+		switch ( $style ) {
+			case 'own':
+				if ( 1 > $color1_opacity ) {
+					$color1 = $this->hex2rgba( $color1, $color1_opacity );
+				}
+				$map = preg_replace( '/"#75c5f0"/', $color1, $map );
+				$map = preg_replace( '/"#9ae095"/', $color1, $map );
+				$map = preg_replace( '/"#fed979"/', $color1, $map );
+				$map = preg_replace( '/"#fffa5e"/', $color1, $map );
+				$map = preg_replace( '/"#f9cdf4"/', $color1, $map );
+				break;
+			case 'auto':
+				$range     = 6;
+				$color1    = $this->hex2hsl( $color1 );
+				$color1[2] = max( ( 2 * $range ), min( ( 100 - 2 * $range ), $color1[2] ) );
+				$color     = sprintf( '"hsl( %d, %d%%, %d%% )"', $color1[0], $color1[1], $color1[2] );
+				$map       = preg_replace( '/"#75c5f0"/', $color, $map );
+				$color     = sprintf( '"hsl( %d, %d%%, %d%% )"', $color1[0], $color1[1], $color1[2] + $range );
+				$map       = preg_replace( '/"#9ae095"/', $color, $map );
+				$color     = sprintf( '"hsl( %d, %d%%, %d%% )"', $color1[0], $color1[1], $color1[2] + ( 2 * $range ) );
+				$map       = preg_replace( '/"#fed979"/', $color, $map );
+				$color     = sprintf( '"hsl( %d, %d%%, %d%% )"', $color1[0], $color1[1], $color1[2] - $range );
+				$map       = preg_replace( '/"#fffa5e"/', $color, $map );
+				$color     = sprintf( '"hsl( %d, %d%%, %d%% )"', $color1[0], $color1[1], $color1[2] - ( 2 * $range ) );
+				$map       = preg_replace( '/"#f9cdf4"/', $color, $map );
+				break;
+		}
+		/**
+		 * styles
+		 */
+		$styles = array(
+			sprintf( 'width:%dpx', $this->options->get_option( 'size' ) ),
+			sprintf( 'height:%dpx', $this->options->get_option( 'size' ) ),
+		);
+
+		$map     = sprintf(
+			'<div class="interactive_polish_map-map" styles="%s">%s</div>',
+			esc_attr( implode( ';', $styles ) ),
+			$map
+		);
+		$content = sprintf(
+			'<div id="ipm_type_%d">%s<ul id="w" class="%s">',
+			get_option( 'ipm_type', 500 ),
+			$map,
+			get_option( 'ipm_menu', 'ponizej' )
+		);
+		/**
+		 * list
+		 */
+		$provinces = $this->get_provinces();
+		$i         = 1;
+		foreach ( $provinces as $key => $value ) {
 			$url = get_option( 'ipm_districts_' . $key, '%' );
 			if ( ! $url ) {
 				$url = '#';
@@ -77,13 +153,136 @@ class iworks_interactive_polish_map extends iworks {
 		return $content;
 	}
 
+	public function admin_init() {
+		iworks_interactive_polish_map_options_init();
+		// add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+	}
 
 	public function init() {
 		wp_register_script( 'interactive_polish_map', plugins_url( '/js/interactive_polish_map.js', __FILE__ ), array( 'jquery' ) );
 		wp_enqueue_script( 'interactive_polish_map' );
 		wp_register_style( 'myStyleSheets', plugins_url( '/style/interactive_polish_map.css', __FILE__ ) );
 		wp_enqueue_style( 'myStyleSheets' );
-		add_filter( 'plugin_row_meta', 'imp_plugin_links', 10, 4 );
 	}
+
+	/**
+	 * cache provinces codes
+	 *
+	 * @since 2.0.0
+	 */
+	private function get_provinces() {
+		if ( empty( $this->provinces ) ) {
+			$this->provinces = $this->options->get_group( 'provinces' );
+		}
+		return $this->provinces;
+	}
+
+	/**
+	 * Plugin logo for rate messages
+	 *
+	 * @since 2.0.0
+	 * @since 1.0.5
+	 *
+	 * @param string $logo Logo, can be empty.
+	 * @param object $plugin Plugin basic data.
+	 */
+	public function filter_plugin_logo( $logo, $plugin ) {
+		if ( is_object( $plugin ) ) {
+			$plugin = (array) $plugin;
+		}
+		if ( 'interactive-polish-map' === $plugin['slug'] ) {
+			return plugin_dir_url( dirname( dirname( __FILE__ ) ) ) . '/assets/images/logo.svg';
+		}
+		return $logo;
+	}
+
+	/**
+	 * get settings page
+	 *
+	 * @since 2.0.0
+	 */
+	public function filter_get_setting_page_url( $url ) {
+		return $this->get_setting_page_url();
+	}
+
+	/**
+	 * get settings page url
+	 *
+	 * @since 2.0.0
+	 */
+	private function get_setting_page_url() {
+		return add_query_arg( 'page', 'ipm_index', admin_url( 'themes.php' ) );
+	}
+
+	private function rgb2array( $color ) {
+		$default = array( 0, 0, 0 );
+		$colors  = array();
+		$color   = preg_replace( '/#/', '', strtolower( $color ) );
+		if ( strlen( $color ) == 6 ) {
+			$colors = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
+		}
+		if ( strlen( $color ) == 3 ) {
+			$colors = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
+		}
+		if ( ! empty( $colors ) ) {
+			return $colors;
+		}
+		return $default;
+	}
+
+	private function hex2rgba( $color, $opacity = false ) {
+		$default = 'rgb(0,0,0)';
+		//Return default if no color provided
+		if ( empty( $color ) ) {
+			return $default;
+		}
+		$rgb = array_map( 'hexdec', $this->rgb2array( $color ) );
+		//Check if opacity is set(rgba or rgb)
+		if ( $opacity ) {
+			if ( abs( $opacity ) > 1 ) {
+				$opacity = 1.0;
+			}
+			$output = 'rgba(' . implode( ',', $rgb ) . ',' . $opacity . ')';
+		} else {
+			$output = 'rgb(' . implode( ',', $rgb ) . ')';
+		}
+		//Return rgb(a) color string
+		return $output;
+	}
+
+	public function hex2hsl( $color ) {
+		$rgb         = array_map( 'hexdec', $this->rgb2array( $color ) );
+		$color_red   = $rgb[0] / 255;
+		$color_green = $rgb[1] / 255;
+		$color_blue  = $rgb[2] / 255;
+		$max         = max( $color_red, $color_green, $color_blue );
+		$min         = min( $color_red, $color_green, $color_blue );
+		$hue;
+		$saturation;
+		$lightness = ( $max + $min ) / 2;
+		$d         = $max - $min;
+		if ( $d == 0 ) {
+			$hue = $saturation = 0; // achromatic
+		} else {
+			$saturation = $d / ( 1 - abs( 2 * $lightness - 1 ) );
+			switch ( $max ) {
+				case $color_red:
+					$hue = 60 * fmod( ( ( $color_green - $color_blue ) / $d ), 6 );
+					if ( $color_blue > $g ) {
+						$hue += 360;
+					}
+					break;
+				case $color_green:
+					$hue = 60 * ( ( $color_blue - $color_red ) / $d + 2 );
+					break;
+				case $color_blue:
+					$hue = 60 * ( ( $color_red - $color_green ) / $d + 4 );
+					break;
+			}
+		}
+
+		return array( round( $hue, 2 ), 100 * round( $saturation, 2 ), 100 * round( $lightness, 2 ) );
+	}
+
 }
 
--- interactive-polish-map.php
+++ interactive-polish-map.php
@@ -14,7 +14,7 @@
  * Domain Path:       /languages
  *
 
-Copyright 2020-PLUGIN_TILL_YEAR Marcin Pietrzak (marcin@iworks.pl)
+Copyright 2013-PLUGIN_TILL_YEAR Marcin Pietrzak (marcin@iworks.pl)
 
 this program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as
@@ -35,9 +35,8 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-$base                                 = dirname( __FILE__ );
-$includes                             = $base . '/includes';
-$iworks_interactive_polish_map_prefix = 'ipm_';
+$base     = dirname( __FILE__ );
+$includes = $base . '/includes';
 
 /**
  * require: Iworksinteractive-polish-map Class
@@ -68,14 +67,21 @@ load_plugin_textdomain( 'interactive-polish-map', false, plugin_basename( $base 
 global $iworks_interactive_polish_map_options;
 $iworks_interactive_polish_map_options = iworks_interactive_polish_map_get_options_object();
 
+/**
+ * run plugin core
+ */
+global $iworks_interactive_polish_map;
+$iworks_interactive_polish_map = new iworks_interactive_polish_map();
+
+
 function iworks_interactive_polish_map_get_options_object() {
-	global $iworks_interactive_polish_map_options, $iworks_interactive_polish_map_prefix;
+	global $iworks_interactive_polish_map_options;
 	if ( is_object( $iworks_interactive_polish_map_options ) ) {
 		return $iworks_interactive_polish_map_options;
 	}
 	$iworks_interactive_polish_map_options = new iworks_options();
 	$iworks_interactive_polish_map_options->set_option_function_name( 'iworks_interactive_polish_map_options' );
-	$iworks_interactive_polish_map_options->set_option_prefix( $iworks_interactive_polish_map_prefix );
+	$iworks_interactive_polish_map_options->set_option_prefix( 'ipm_' );
 	return $iworks_interactive_polish_map_options;
 }
 
@@ -85,25 +91,16 @@ function iworks_interactive_polish_map_options_init() {
 }
 
 function iworks_interactive_polish_map_activate() {
-	global $iworks_interactive_polish_map_prefix;
 	$iworks_interactive_polish_map_options = new iworks_options();
 	$iworks_interactive_polish_map_options->set_option_function_name( 'iworks_interactive_polish_map_options' );
-	$iworks_interactive_polish_map_options->set_option_prefix( $iworks_interactive_polish_map_prefix );
+	$iworks_interactive_polish_map_options->set_option_prefix( 'ipm_' );
 	$iworks_interactive_polish_map_options->activate();
-	/**
-	 * install tables
-	 */
-	$iworks_interactive_polish_map = new iworks_interactive_polish_map;
-	$iworks_interactive_polish_map->db_install();
 }
 
 function iworks_interactive_polish_map_deactivate() {
 	global $iworks_interactive_polish_map_options;
 	$iworks_interactive_polish_map_options->deactivate();
 }
-
-global $iworks_interactive_polish_map;
-$iworks_interactive_polish_map = new iworks_interactive_polish_map();
 
 /**
  * install & uninstall
@@ -120,166 +117,4 @@ do_action(
 	__( 'Interactive Polish Map', 'interactive-polish-map' ),
 	'interactive-polish-map'
 );
-
-return;
-/*
-Plugin Name:
-Plugin URI: http://wordpress.org/extend/plugins/interactive-polish-map/
-Description: Interactive Polish Map display Polish map using shortcode or widget.
-Version: trunk
-Author: Marcin Pietrzak
-Author URI: http://iworks.pl
-*/
-
-/**
- * $HeadURL: https://plugins.svn.wordpress.org/interactive-polish-map/trunk/interactive-polish-map.php $
- * $LastChangedBy: iworks $
- * $LastChangedDate: 2015-08-08 10:14:03 +0200 (Sat, 08 Aug 2015) $
- */
-
-/**
- * i18n
- */
-$mo_file = dirname( __FILE__ ) . '/languages/' . get_locale() . '.mo';
-if ( file_exists( $mo_file ) && is_readable( $mo_file ) ) {
-	load_textdomain( 'interactive_polish_map', $mo_file );
-}
-
-$ipm_data = array(
-	'districts' => array(
-		'dolnoslaskie'        => 'Województwo Dolnośląskie',
-		'kujawsko_pomorskie'  => 'Województwo Kujawsko-Pomorskie',
-		'lubelskie'           => 'Województwo Lubelskie',
-		'lubuskie'            => 'Województwo Lubuskie',
-		'lodzkie'             => 'Województwo Łódzkie',
-		'malopolskie'         => 'Województwo Małopolskie',
-		'mazowieckie'         => 'Województwo Mazowieckie',
-		'opolskie'            => 'Województwo Opolskie',
-		'podkarpackie'        => 'Województwo Podkarpackie',
-		'podlaskie'           => 'Województwo Podlaskie',
-		'pomorskie'           => 'Województwo Pomorskie',
-		'slaskie'             => 'Województwo Śląskie',
-		'swietokrzyskie'      => 'Województwo Świętokrzyskie',
-		'warminsko_mazurskie' => 'Województwo Warmińsko-Mazurskie',
-		'wielkopolskie'       => 'Województwo Wielkopolskie',
-		'zachodniopomorskie'  => 'Województwo Zachodniopomorskie',
-	),
-	'menu'      => array(
-		'ukryta'               => array(
-			'widget' => true,
-			'desc'   => __( 'hidden', 'interactive_polish_map' ),
-		),
-		'po_lewej'             => array(
-			'widget' => false,
-			'desc'   => __( 'on left', 'interactive_polish_map' ),
-		),
-		'po_prawej'            => array(
-			'widget' => false,
-			'desc'   => __( 'on right', 'interactive_polish_map' ),
-		),
-		'ponizej'              => array(
-			'widget' => true,
-			'desc'   => __( 'under', 'interactive_polish_map' ),
-		),
-		'ponizej dwie_kolumny' => array(
-			'widget' => false,
-			'desc'   => __( 'under - two columns (only for 400px & 500px)', 'interactive_polish_map' ),
-		),
-		'ponizej trzy_kolumny' => array(
-			'widget' => false,
-			'desc'   => __( 'under - three columns (only for 500px)', 'interactive_polish_map' ),
-		),
-	),
-	'type'      => array(
-		'200' => array(
-			'widget' => true,
-			'desc'   => '200px',
-		),
-		'300' => array(
-			'widget' => true,
-			'desc'   => '300px',
-		),
-		'400' => array(
-			'widget' => false,
-			'desc'   => '400px',
-		),
-		'500' => array(
-			'widget' => false,
-			'desc'   => '500px',
-		),
-	),
-);
-
-
-
-function ipm_produce_radio( $name, $title, $options, $default ) {
-	$option_value = get_option( $name, $default );
-	$content      = sprintf( '<h3>%s</h3>', $title );
-	$content     .= '<ul>';
-	$i            = 0;
-	foreach ( $options as $value => $data ) {
-		$id = $name . $i++;
-		if ( isset( $option['name'] ) ) {
-			$id = $name . $option['name'] . $i++;
-		}
-		$content .= sprintf(
-			'<li><label for="%s"><input type="radio" name="%s" value="%s"%s id="%s"/> %s</label></li>',
-			$id,
-			$name,
-			$value,
-			( $option_value == $value ) ? ' checked="checked"' : '',
-			$id,
-			$data['desc']
-		);
-	}
-	$content .= '</ul>';
-	echo $content;
-}
-
-function ipm_settings() {
-	global $ipm_data;
-	?>
-<div class="wrap">
-	<h2><?php _e( 'Interactive Polish Map', 'interactive_polish_map' ); ?></h2>
-	<form method="post" action="options.php">
-	<?php
-	ipm_produce_radio( 'ipm_type', __( 'Map width', 'interactive_polish_map' ), $ipm_data['type'], 500 );
-	ipm_produce_radio( 'ipm_menu', __( 'Display list', 'interactive_polish_map' ), $ipm_data['menu'], 'ponizej' );
-	?>
-		<h3><?php _e( 'URL' ); ?></h3>
-		<table class="widefat">
-	<?php
-	$i = 1;
-	foreach ( $ipm_data['districts'] as $key => $value ) {
-		$url = get_option( 'ipm_districts_' . $key, '' );
-		printf(
-			'<tr%s><td style="width:150px">%s:</td><td><input type="text" name="ipm_districts_%s" value="%s" class="widefat"/></td></tr>',
-			++$i % 2 ? ' class="alternate"' : '',
-			$value,
-			$key,
-			$url
-		);
-	}
-	settings_fields( 'ipm-options' );
-	?>
-		</table>
-		<p class="submit"><input type="submit" class="button-primary" value="<?php _e( 'Save Changes', 'interactive_polish_map' ); ?>" /></p>
-	</form>
-</div>
-	<?php
-}
-
-
-function imp_plugin_links( $plugin_meta, $plugin_file, $plugin_data, $status ) {
-	if ( strpos( $plugin_file, basename( __FILE__ ) ) ) {
-		$plugin_meta[] = '<a href="options-general.php?page=ipm_settings">' . __( 'Settings' ) . '</a>';
-		$plugin_meta[] = '<a href="http://iworks.pl/donate/ipm.php">' . __( 'Donate' ) . '</a>';
-	}
-	return $plugin_meta;
-}
-
-/**
- * load snippets
- */
-include_once dirname( __FILE__ ) . '/snippets/widget_map.php';
 
